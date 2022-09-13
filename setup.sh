3a8d7051d1f22d34e9e3d048a9feb236e6d08c70
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-09-13 10:46:32 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-while true; do
-    read -p "Do you wish to install this program? " yn
-    case $yn in
-        [Yy]* ) echo "installed"; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
