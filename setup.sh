f57b24fcc26df712451caef34bf353b7b0d3b901
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-09-13 14:17:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-printf 'Is this a good question (y/n)? '
+echo 'Is this a good question (y/n)? '
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
     echo Yes
