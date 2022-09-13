1611ffdb17d3789fb1b297e202957da8c58041a3
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-09-13 15:15:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-function dumpData() {	
+
 	read -p "System? (prod/stg) default production: " system;
 	case $system in
 	"prod")
@@ -8,15 +8,6 @@ function dumpData() {
 	*)
 	SYSTEM="production";;
 	esac
-    }
+    
 
-MODE=$1
-if [ $MODE != "dump" ] && [ $MODE != "load" ]; then
-    echo "Invalid mode, use dump or load"
-    exit 1
-fi
-if [[ $MODE = "dump" ]]; then
-    dumpData
-else
-    loadData
-fi
+
