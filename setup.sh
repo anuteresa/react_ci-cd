a104ecb0f43a12c8cee381629f14d8b2a9237a36
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-07 15:04:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-function dumpData() {	
-	read -p "System? (prod/stg) default production: " system;
-	case $system in
-	"prod")
-	read -p "Name? (prod/stg) default production: " name;
-	
-	"stg")
-	pwd;;
-	
-  esac
-}
-
-MODE=$1	
-	if [ $MODE != "dump" ] && [ $MODE != "load" ]; then
-	echo "Invalid mode, use dump or load"
-	exit 1
-	fi
-	if [[ $MODE = "dump" ]]; then
-	dumpData
-	else
-	loadData
-	fi
-
+read -p 'Username: ' uservar
+read -sp 'Password: ' passvar
+echo
+echo Thankyou $uservar we now have your login details
     
 
 
