218ca93b59930b07a460f86ebf1756cf6ecde59b
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-07 12:34:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and all
+          Learn React and all and enjoy
         </a>
       </header>
     </div>
