783961a59bd65e0abae29a0cec6e5c9a38a9a91f
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-11 17:00:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ OLD_HEAD=$(git rev-parse HEAD)
 git pull --dry-run https://anuteresa:$token@github.com/anuteresa/react_ci-cd
 NEW_HEAD=$(git rev-parse HEAD)
 echo $NEW_HEAD
-ls -la |
+if [ $OLD_HEAD = $NEW_HEAD ] | ls -la
 while read name; do
   if [ "$name" = "setup.sh" ];
    then
@@ -15,6 +15,7 @@ while read name; do
                   
   fi;
 done  
+fi
 
 
                  
