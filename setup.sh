a3e3d7856b2b9e19553160ea141968940ff86e42
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-11 15:09:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-read -p 'Username: ' uservar
-read -sp 'Password: ' passvar
-echo than
-echo Thankyou $uservar we now have your login details
+OLD_HEAD=$(git rev-parse HEAD)
+git pull https://anuteresa:${{secrets.my_pat}}@github.com/anuteresa/react_ci-cd
+NEW_HEAD=$(git rev-parse HEAD)
+git diff --name-only $OLD_HEAD $NEW_HEAD |
+  while read name; do
+  if [ "$num" = "Pipfile.lock" ];
+   then
+       echo "good"
+       else 
+       echo "not goood
+                  
+    fi;
+    done  
+                 
     
 
 
