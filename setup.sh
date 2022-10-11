98e1ff54e1b09aa55521721819c381a9c79f8a0d
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-11 15:50:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OLD_HEAD=$(git rev-parse HEAD)
-git pull https://anuteresa:$token@github.com/anuteresa/react_ci-cd
+git pull --dry-run https://anuteresa:$token@github.com/anuteresa/react_ci-cd
 NEW_HEAD=$(git rev-parse HEAD)
 echo $NEW_HEAD
 git diff --name-only $OLD_HEAD $NEW_HEAD |
