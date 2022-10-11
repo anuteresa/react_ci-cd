dd7b216ec60119fa0356833c88df09fdb8e65e73
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-11 17:04:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,17 +4,18 @@ git pull --dry-run https://anuteresa:$token@github.com/anuteresa/react_ci-cd
 NEW_HEAD=$(git rev-parse HEAD)
 echo $NEW_HEAD
 ls -la | 
-while read name; do
+while read name; do 
   if [ "$name" = "setup.sh" ];
    then
        echo "good"
        pwd
- done      
+      
   else 
        echo "not goood";
        pwd
                   
   fi;
+  done
 
 
                  
