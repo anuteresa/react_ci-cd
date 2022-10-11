733d86f5dcee4de7cc0a489a376a6a801f20adbb
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-11 16:54:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,20 +3,18 @@ OLD_HEAD=$(git rev-parse HEAD)
 git pull --dry-run https://anuteresa:$token@github.com/anuteresa/react_ci-cd
 NEW_HEAD=$(git rev-parse HEAD)
 echo $NEW_HEAD
-git diff --name-only $OLD_HEAD $NEW_HEAD |
+ls -la |
 while read name; do
-  if [ "$num" = "Pipfile.lock" ];
+  if [ "$num" = "setup.sh" ];
    then
        echo "good"
   else 
-       echo "not goood"
+       echo "not goood";
+       pwd
                   
   fi;
 done  
-if [ $OLD_HEAD = $NEW_HEAD ];
-then 
-echo "no changes and happy"
-fi
+
 
                  
     
