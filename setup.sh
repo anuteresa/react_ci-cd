1385e1a2e8bb679160e46f4d7c85a87f9413cfcd
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-11 15:13:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,17 @@
 OLD_HEAD=$(git rev-parse HEAD)
 git pull 
 NEW_HEAD=$(git rev-parse HEAD)
+echo $NEW_HEAD
 git diff --name-only $OLD_HEAD $NEW_HEAD |
-  while read name; do
+while read name; do
   if [ "$num" = "Pipfile.lock" ];
    then
        echo "good"
-       else 
+  else 
        echo "not goood"
                   
-    fi;
-    done  
+  fi;
+done  
                  
     
 
