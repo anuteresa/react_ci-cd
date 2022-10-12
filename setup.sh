1b1f96aab9106edd64440fe057882c5d9b82121b
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2022-10-12 12:34:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@ OLD_HEAD=$(git rev-parse HEAD)
 git pull
 NEW_HEAD=$(git rev-parse HEAD)
 echo $NEW_HEAD
-result=$(ls | egrep 'setup.sh') 
+result=$(ls | egrep 'anu.sh') 
 echo $result
-if [ "$result" = "setup.sh" ]
+if [ "$result" = "anu.sh" ]
 then
   echo 'found'
+else 
+echo 'not found'
 fi  
 
 if [ $OLD_HEAD = $NEW_HEAD ]
