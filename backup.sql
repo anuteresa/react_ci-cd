4c9a821c59e147f00e096ed115756e6710e16474
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2023-01-10 13:18:52 +0100

    Create backup.sql

---
+++ backup.sql
@@ -0,0 +1,2 @@
+CREATE TABLE phonebook(phone VARCHAR(32), firstname VARCHAR(32), lastname VARCHAR(32), address VARCHAR(64));
+INSERT INTO phonebook(phone, firstname, lastname, address) VALUES('+1 123 456 7890', 'John', 'Doe', 'North America');
