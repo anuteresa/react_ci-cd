3449dde25819018b2222d27f68593b326ee102a9
ANU TERESA JOSE <66132119+anuteresa@users.noreply.github.com>
2023-01-10 13:14:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-OLD_HEAD=$(git rev-parse HEAD)
-git pull git pull https://anuteresa:Attikal123@github.com/anuteresa/react_ci-cd
-NEW_HEAD=$(git rev-parse HEAD)
-echo $NEW_HEAD
-result=$(ls | egrep 'setup.sh') 
-
-if [ "$result" = "setup.sh" ]
-then
-  echo 'found and be happy'
-else 
-echo 'not found and anu'
-fi  
-
-if [ $OLD_HEAD = $NEW_HEAD ]
-then 
- pwd;
- echo "happy";
- fi
+python3 upload.py
 
 
 
